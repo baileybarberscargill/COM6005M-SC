6b49c2ec5361a53c29b3a71caee4ba71cda9833a
Bailey <baileybs@live.co.uk>
2023-01-17 01:01:02 +0000

    Added wifi settings file

---
+++ Arduino-Vehicle/wifi_settings.h
@@ -0,0 +1,6 @@
+#define SECRET_WIFI "The Promised LAN"
+#define SECRET_PASS "cnQbkqsy2xnf"
+
+#define SECRET_mqttBroker "broker.hivemq.com"
+#define SECRET_mqttPort 1883;
+#define SECRET_mqttTopic "tripData/data"; 
